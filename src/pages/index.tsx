d1b026dcdbf831f2fbc4e9c13114e1153069a704
Jezebel1990 <jezebelguedes@gmail.com>
2024-08-04 19:55:54 -0300

    add foo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ type Props = {
   title: string
 }
 
+const foo = 123
+
 export default function Home({ title = 'React Avançado!' }: Props) {
   return (
     <div>
-      <h1>{title} </h1>
+      <h1>{title}</h1>
     </div>
   )
 }
